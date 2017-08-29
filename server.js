4e5b14c174f85f156a8df196ff429ba792bf2b65
qinghang <qinghangzeng@gmail.com>
2017-08-29 11:35:58 -0400

    adjust setting

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
 
-mongoose.connect(process.env.MONGOLAB_URI, {useMongoClient: true});
-
+app.set('port', (process.env.PORT || 3001));
+app.enable('trust proxy');
 //serve the react app files
 app.use(express.static(`${__dirname}/ui-react/build`));
+
+mongoose.connect(process.env.MONGOLAB_URI, {useMongoClient: true});
 
 require('./config/passport')(passport); //pass passport for configuration
 //required for passport
@@ -19,13 +21,15 @@ app.use(passport.session());
 
 require('./routes/auth.js')(app, passport);
 
-app.set('port', (process.env.PORT || 3001));
-app.enable('trust proxy');
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(require('./routes/api.js'));
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response){
+    response.sendFile(`${__dirname}/ui-react/build/index.html`);
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
